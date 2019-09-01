cd73af2a9d47fa68abec377d216d6da15e092b17
kim2039 <nohara1016@gmail.com>
2019-09-01 15:04:39 +0900

    batファイルを追加

---
+++ motion_trace_bulk-master/BulkDepth.bat
@@ -0,0 +1,23 @@
+@echo off
+rem --- 
+rem ---  映像データから深度推定を行う
+rem --- 
+
+echo ------------------------------------------
+echo FCRN-DepthPrediction-vmd
+echo ------------------------------------------
+
+rem -- FCRN-DepthPrediction-vmd ディレクトリに移動
+cd /d %~dp0
+cd /d %DEPTH_DIR%
+
+rem ---  json 処理
+python complement_body.py %OUTPUT_JSON_DIR%
+
+rem ---  python 実行
+
+python tensorflow/predict_video.py --model_path %FCRN_MODEL% --video_path %INPUT_VIDEO% --json_path %OUTPUT_JSON_DIR% --past_depth_path "%PAST_DEPTH_PATH%" --interval 1 --reverse_specific "%REVERSE_SPECIFIC_LIST%" --order_specific "%ORDER_SPECIFIC_LIST%" --avi_output yes --verbose %VERBOSE% --number_people_max %NUMBER_PEOPLE_MAX% --end_frame_no %FRAME_END% --now %DTTM%
+
+cd /d %~dp0
+
+exit /b
